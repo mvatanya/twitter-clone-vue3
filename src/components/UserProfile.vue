09c391b40b2e95e84a3352b8647d8ddd8630c429
Marisa Vatanya <mvatanya@gmail.com>
2020-09-20 15:35:25 -0700

    setup project and created UserProfile component

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__follower-count">
+        <strong>Followers:</strong>
+        {{followers}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "mRomney",
+        firstName: "Mitchell",
+        lastName: "Romney",
+        email: "mromney@email.com",
+        isAdmin: true,
+      },
+    };
+  },
+  // watch is for watching when data change, then it will run the function in here
+  watch: {
+    // this followers function will run when followers data changes
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follwer!`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3EB;
+}
+
+h1 {
+  margin: 0
+}
+</style>
